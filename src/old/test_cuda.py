cf2ac328e38788149cc1aca77506b2ef0e01b17b
Dmitry-pharma <mrsy@yandex.ru>
2025-10-16 23:12:50 +0300

    Delete src/old directory

--- src/old/test_cuda.py
+++
@@ -1,7 +0,0 @@
-import torch
-print(torch.__version__)
-print(torch.version.cuda)
-print("CUDA available:", torch.cuda.is_available())
-print("CUDA device count:", torch.cuda.device_count())
-print("Current device:", torch.cuda.current_device())
-print("Device name:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "N/A")
